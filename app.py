b30f6568a766200e2eb38d9268e0b95a98f4a1ce
Damián <admin@damianverde.es>
2023-06-11 21:41:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,22 @@ from io import BytesIO
 app = Flask(__name__)
 
 @app.route('/tts', methods=['POST'])
-def tts():
-    text = request.form['text']
-    tts = gTTS(text=text, lang='en')
-    mp3_fp = BytesIO()
-    tts.write_to_fp(mp3_fp)
-    mp3_fp.seek(0)
-    return Response(mp3_fp, mimetype='audio/mpeg')
+def text_to_speech():
+    # Check if 'text' is present in the request form
+    if 'text' not in request.form:
+        return Response("Missing 'text' parameter in request form", status=400)
+
+    input_text = request.form['text']
+    
+    # Check if the input text is empty
+    if not input_text.strip():
+        return Response("Input text cannot be empty", status=400)
+
+    tts_engine = gTTS(text=input_text, lang='en')
+    mp3_buffer = BytesIO()
+    tts_engine.write_to_fp(mp3_buffer)
+    mp3_buffer.seek(0)
+    return Response(mp3_buffer, mimetype='audio/mpeg')
 
 if __name__ == '__main__':
     app.run(port=8089)
